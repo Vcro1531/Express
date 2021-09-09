4bce5248578b65ce41ee50bc6d6b7165c753832c
KimJiMin <minmin4736@naver.com>
2021-09-09 23:22:33 +0900

    📑 ::struct

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var router = express.Router();
 var template = require('../lib/template.js');
- 
-router.get('/', function(request, response) { 
-    var title = 'Welcome';
-    var description = 'Hello, Node.js';
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
+
+router.get('/', function (request, response) {
+	var title = 'Welcome';
+	var description = 'Hello, Node.js';
+	var list = template.list(request.list);
+	var html = template.HTML(
+		title,
+		list,
+		`
       <h2>${title}</h2>${description}
       <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
       `,
-      `<a href="/topic/create">create</a>`
-    ); //사진이 오류 나서 안보여요 TT
-    response.send(html);
-  });
-   
-  module.exports = router;
+		`<a href="/topic/create">create</a>`
+	); //사진이 오류 나서 안보여요 TT
+	response.send(html);
+});
+
+module.exports = router;
